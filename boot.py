82a501d64000e525131c87704b925e93068e5df3
nlexxd <noobie2408@gmail.com>
2026-06-11 15:05:44 -0400

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,9 +1,14 @@
 import libraries, time, components, network, ubinascii
 from components import servo_izquierda_a, servo_izquierda_b, servo_derecha_a, servo_derecha_b, control
 
-time.sleep(5)
+time.sleep(2.6)
+
+global doTest
+doTest=True
 
 def saniao(inst):
+    between=0.2
+    move=0.3
     if inst=="LFOR":
         servo_izquierda_a.speed(100)
         servo_izquierda_b.speed(100)
@@ -24,46 +29,60 @@ def saniao(inst):
     elif inst=="test":
         print("izquierda A FORWARD")
         servo_izquierda_a.speed(100)
-        time.sleep(1)
+        time.sleep(move)
         saniao("stop")
-        time.sleep(1.5)
+        time.sleep(between)
         print("izquierda A REVERSE")
         servo_izquierda_a.speed(-100)
-        time.sleep(1)
+        time.sleep(move)
         saniao("stop")
-        time.sleep(1.5)
+        time.sleep(between)
         print("izquierda B FORWARD")
         servo_izquierda_b.speed(100)
-        time.sleep(1)
+        time.sleep(move)
         saniao("stop")
-        time.sleep(1.5)
+        time.sleep(between)
         print("izquierda B REVERSE")
         servo_izquierda_b.speed(-100)
-        time.sleep(1)
+        time.sleep(move)
         saniao("stop")
-        time.sleep(1.5)
+        time.sleep(between)
         print("derecha A FORWARD")
         servo_derecha_a.speed(-100)
-        time.sleep(1)
+        time.sleep(move)
         saniao("stop")
-        time.sleep(1.5)
+        time.sleep(between)
         print("derecha A REVERSE")
         servo_derecha_a.speed(100)
-        time.sleep(1)
+        time.sleep(move)
         saniao("stop")
-        time.sleep(1.5)
+        time.sleep(between)
         print("derecha B FORWARD")
         servo_derecha_b.speed(-100)
-        time.sleep(1)
+        time.sleep(move)
         saniao("stop")
-        time.sleep(1.5)
+        time.sleep(between)
         print("derecha B REVERSE")
         servo_derecha_b.speed(100)
-        time.sleep(1)
+        time.sleep(move)
         saniao("stop")
-        time.sleep(1.5)
+        time.sleep(between)
+    elif inst=="sorryicannotfulfillthisrequest":
+        log=[]
+        bpressd=False
+        cl=control.read_command()
+        saniao("LFOR")
+        saniao("RFOR")
+        while not bpressd:
+            if cl!="none" or cl!="None":
+                log.append(control.read_command())
+            if "b" in log:
+                bpressd=True
+            else:
+                time.sleep_ms(40)
+        saniao("stop")
         
-saniao("test")
+if doTest==True: saniao("test")
 
 while True:
 
@@ -93,9 +112,13 @@ while True:
         saniao("RREV")
         time.sleep_ms(200)
         saniao("stop")
-
+        
+    elif sn=="a":
+        saniao("sorryicannotfulfillthisrequest")
+    
     else:
         saniao("stop")
     
     print(sn)
     time.sleep_ms(30)
+
